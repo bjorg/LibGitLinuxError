c1b21d1b2cb28c2522deb53f31d8613e4288a5a7
root <root@STEVEBJORG-DESKTOP.localdomain>
2021-10-09 13:12:54 -0700

    clean-up

--- Program.cs
+++ Program.cs
@@ -2,12 +2,12 @@
 using System.IO;
 using LibGit2Sharp;
 
-namespace LibGitTrouble
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace LibGitTrouble {
+
+    public class Program {
+
+        //--- Class Methods ---
+        public static void Main(string[] args) {
             var gitUrl = "https://github.com/LambdaSharp/AlexaEcho.git";
             var folder = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
             Console.WriteLine($"Cloning {gitUrl} to {folder}");
