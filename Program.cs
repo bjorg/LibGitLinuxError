3287766e6a06c8ecc321e66d723ac221adea1363
Steve G. Bjorg <steve.bjorg@gmail.com>
2021-10-09 13:28:12 -0700

    use .NET Core 3.1

--- Program.cs
+++ Program.cs
@@ -7,11 +7,7 @@ namespace LibGitTrouble {
     public class Program {
 
         //--- Class Methods ---
-        public static void Main(string[] args) {
-            var gitUrl = "https://github.com/LambdaSharp/AlexaEcho.git";
-            var folder = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
-            Console.WriteLine($"Cloning {gitUrl} to {folder}");
-            string clonedRepoPath = Repository.Clone(gitUrl, folder);
-        }
+        public static void Main(string[] args)
+            => Console.WriteLine($"Current directory is a git folder: {Repository.IsValid(Directory.GetCurrentDirectory())}");
     }
 }
